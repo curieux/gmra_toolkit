9ab9b7ee2a680c302554823024b6c0bdc6c74634
Abraham D. Smith <abe@curieux.us>
2015-03-02 09:36:56 -0500

    playing with different svd methods

--- data_manifold_tree.py
+++ data_manifold_tree.py
@@ -15,7 +15,7 @@ class DataManifoldTree(object):
 
 
     
-    def __init__(self,data,length=scipy.spatial.distance.pdist):
+    def __init__(self,data,length=scipy.spatial.distance.cdist):
         """ data is a numpy.ndarray containing the sample points as rows.
             That is, data.shape=(n,D) means there are n sample points in RR^D.
         """
@@ -23,14 +23,13 @@ class DataManifoldTree(object):
         DataManifoldTree.valid_data(data)
 
         self.data=data
-        self.samples,self.measurements=self.data.shape
+        self.samples,self.instruments=self.data.shape
 
         self.length=length
-        self.vdistances=self.length(self.data)
-        self.diameter=numpy.amax(self.vdistances)     
-        self.distances=scipy.spatial.distance.squareform(self.vdistances)
-
-
+        self.distances=self.length(self.data,self.data)
+        self.diameter=numpy.amax(self.distances)     
+        ### this only works for Euclidean distance!!!
+        self.tree=scipy.spatial.cKDTree(self.data)
 
     def random_pt(self):
         """ select a random point from our data """
@@ -50,38 +49,59 @@ class DataManifoldTree(object):
         return self.data.__getitem__(key)
 
     def __str__(self):
-        return "DataManifoldTree with {} samples of {} measurements".format(self.samples,self.measurements)
+        return "DataManifoldTree with {} samples of {} instruments".format(self.samples,self.instruments)
         
 
-    def neighbors(self,i,radius=None):
+    def view_from_indices(self,indices):
+        """ make a mask of the data from a list of "good" indices """
+        nbrs=self.data.view(numpy.ma.MaskedArray)
+        mask = [ [True]*self.instruments for j in range(self.samples) ]
+        for i in indices:
+            mask[i]=[False]*self.instruments
+        nbrs.mask=numpy.array(mask)
+        assert nbrs.mask.shape == self.data.shape
+        return nbrs
+
+    def copy_from_indices(self,indices):
+        """ make a mask of the data from a list of "good" indices """
+        nbrs=self.data[tuple(indices),:]
+        return DataManifoldTree(nbrs)
+
+    def neighbors(self,i,radius):
         """ list rows in self.data that are neighbors of sample within radius.
 
-            radius defaults to None, meaning `infinity'
+            radius defaults to -1, meaning `infinity' (really `2*diameter')
 
-            We return a list instead of an ndarray because otherwise fancy
+            We return a masked ndarray view instead of an ndarray because otherwise fancy
             indexing would force copies, not views, and self.data is probably huge.
-            Feed this output to numpy.array( ) in case you want to operate on
-            it, at the cost of memory. 
             """
-        if not radius: radius=2*self.diameter  ## catch EVERYTHING 
-        dist_from_i=self.distances[i]
-        return [ self[j] for j in range(self.measurements) if self.distances[i][j] <= radius ]
-#         return [ self[j] for i in self.find(sample,tolerance=radius) ]
-            
 
-    def svd(self, point, subset=None):
-        """ perform svd on data set versus a point.  
-        Can restrict data to a subset given as rows of an ndarray. 
-        input point is used as the centroid of the new data.  """ 
+        if radius==-1: radius=2*self.diameter
+        return self.tree.query_ball_point(self.data[i], radius)
 
-        if not subset: subset=self.data
 
-        ## Do I want to ASSUME the point is the center, or allow translations?
+    def svd(self, point=None, centroid='point'): #maskedview=None,valsonly=True):
+        """ perform svd on data set (versus a point).  
+        If mean=='point' (default), input point is used as the centroid of the new data. 
+        If mean=='mean', then the mean of the data is used as the centroid.
+        If mean=='none', then the data is used on its own, in the ambient coordinates.
+        """ 
+
         import scipy.linalg
-        subsamples=subset.shape[0]
-        affine=subset - numpy.tile(point,(subsamples,1))
-        return scipy.linalg.svd(affine)
 
+        if centroid=='point' and isinstance(point,numpy.ndarray) \
+          and point.ndim==1 and point.shape==(self.instruments,):
+            centroidv=point
+
+        elif centroid=='mean':
+            centroidv=self.data.mean(axis=0) 
+        elif centroid=='none':
+            centroidv=0
+        else:
+            raise ValueError("centroid must be 'mean', 'none', or 'point'.  If 'point', provide a point as an ndarray!")
+
+        affine=(1/numpy.sqrt(self.samples -1))*(self.data - centroidv)
+        return scipy.linalg.svdvals(affine)
 
 if __name__ == "__main__":
     print "Hello"
@@ -93,11 +113,14 @@ if __name__ == "__main__":
     print A.data
     i,p=A.random_pt()
     print A.data is p.base
-    U,s,V=A.svd(p)
+    s=A.svd(p)
     print s
+#    print A.tree.query(p, 2)
     print A.distances
-    print A.diameter
-    b=numpy.array([0,0,1])
+   
+   
+    #print A.diameter
+    #b=numpy.array([0,0,1])
     #print A.find(b,2)
     #An=A.neighbors(b,2)
     #print type(An[0].base)
@@ -113,24 +136,57 @@ if __name__ == "__main__":
             10*numpy.sin(theta)+numpy.random.rand()])
     data=numpy.array(data)
 
-    import matplotlib.pyplot
-    d=matplotlib.pyplot.scatter(data[:,0],data[:,1])
-    d.show()
-    print data.shape
+    #import matplotlib.pyplot
+        #d=matplotlib.pyplot.scatter(data[:,0],data[:,1])
+    #d.show()
+    #print data.shape
     print "Now, the slow part"
     C=DataManifoldTree(data)
-    print C.data.shape
-    print C.distances.shape
+    #print C.data.shape
+    #print C.distances.shape
     print C.diameter
     print C.distances.mean()
-
-    for i in xrange(5):
-        j,p=C.random_pt()
-        pn=C.neighbors(j,5.0)
-        print
-        print p
-        print pn
+#
+    j,p=C.random_pt() #0,C[0]
+    print p
+    for i in [1,5,20,50,100,200,250]:
         print 
+#        print 0.1*i
+        pn=C.neighbors(j,0.1*i)
+        pnp=C.copy_from_indices(pn)
+        pd=numpy.array( [ C.distances[j,k] for k in pn ])
+#        print "#{}: {} -- {}".format(len(pn), numpy.amin(pd), numpy.amax(pd))
+#        print scipy.linalg.svdvals(pnp)
+        #bias=numpy.amax(maskeddistances)     
+#        print pnp.diameter
+        s=pnp.svd(p)
+        print 0.1*i, s
+
+    CC=1/float(C.samples-1)*(C.data.T).dot(C.data)
+    print numpy.sqrt(scipy.linalg.eig(CC)[0])
+    print C.diameter
+    print C.svd(centroid='mean')
+#,pnp)
+#        print type(pnp), pnp.count
+        #C.distances[j, (pn) ]), 
+        #, numpy.amax(C.distances[
+            #print C[idx]
+        #affine=(pn - numpy.tile(p,(C.samples,1)))
+        #print pn[~pn.mask]
+        #print C.svd(p,pn)
+
+
+    #print pn.shape
+    #print pn.mask
+#print pn[~pn.mask]
+#for index, i, in numpy.ndenumerate(pn[~pn.mask]): 
+#        print index, i, k
+
+#    for q in pn:
+#        print q
+    #    print p
+#    print pn.mean()
+    #    print 
 #print C.i
 
 #    D=C.data.dot(C.data.transpose())
